d9570438faf429c1d762b3e357f2df6ae43a63df
NEXT29297 <Porawat.sithkankiat@gmail.com>
2025-04-21 00:44:50 +0700

    Fix link with data in User and sales

--- ShinEdit/GetData/usersdata.py
+++ ShinEdit/GetData/usersdata.py
@@ -7,7 +7,7 @@ def get_usersdata(cur):
     result = cur.fetchall()
 
     # Convert the result to a pandas DataFrame
-    usersdata = pd.DataFrame(result, columns = ['Username', 'SalesPersonID', 'Password', 'DOB', 'Gender', 'Position', 'PhoneNumber'])
+    usersdata = pd.DataFrame(result, columns = ['Username', 'SalesPersonID','Password', 'Nickname', 'Email'])
 
     return usersdata
 
@@ -15,13 +15,12 @@ def get_display_users(cur):
 
     # SQL part: Get data from the table in database
     cur.execute("""
-            SELECT Username, SalesPerson.SalesPersonID, SalesName, DOB, Gender, Position, PhoneNumber FROM Users
+            SELECT Username, SalesPerson.SalesPersonID, Password, SalesName, Nickname, Email FROM Users
             JOIN SalesPerson ON Users.SalesPersonID = SalesPerson.SalesPersonID ORDER BY Username;
             """)
     display_sql = cur.fetchall()
 
     # Convert the result to a pandas DataFrame
-    display_data = pd.DataFrame(display_sql, columns = ['Username', 'Sales Person ID', 'Sales Name', 'Date of Birth',
-                                                        'Gender', 'Position', 'Phone Number'])
+    display_data = pd.DataFrame(display_sql, columns = ['Username', 'Sales Person ID','Password', 'Sales Name', 'nickname', 'email'])
 
     return display_data
