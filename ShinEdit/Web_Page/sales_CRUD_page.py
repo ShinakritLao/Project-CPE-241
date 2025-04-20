138442ffaa8da63076a4fe0fe95bdca492da65cf
KantadaP <kantada.petc@mail.kmutt.ac.th>
2025-04-20 23:08:19 +0700

    Add clear history

--- ShinEdit/Web_Page/sales_CRUD_page.py
+++ ShinEdit/Web_Page/sales_CRUD_page.py
@@ -13,25 +13,27 @@ def Sales_CRUD(cur, conn, salesperson, all_sales_data, display_data):
     # Load existing sales data
     st.dataframe(display_data)
 
+    # Add tab for add update delete
     add_record, update_record, delete_record = st.tabs(['Add', 'Update', 'Delete'])
 
-    # Add record
+    # Add record to the table
     with add_record:
+
         # Increase sales id
         if len(all_sales_data) == 0:
             new_value = "S001"
         else:
             last_value = all_sales_data['SalesID'].iloc[-1]
-
             prefix = ''.join(filter(str.isalpha, last_value))
             number = ''.join(filter(str.isdigit, last_value))
-
             new_number = str(int(number) + 1).zfill(len(number))
             new_value = prefix + new_number
 
-        # Form to add new record
         st.subheader("Add New Sale Record")
+
+        # Form to add new record
         with st.form("Add Record"):
+
             new_sale_id = st.text_input("Sales ID", value = new_value, disabled = True)
             insert_salesperson = st.selectbox("Sales Person ID", salesperson)
             new_quantity = st.number_input("Number of product")
@@ -40,6 +42,7 @@ def Sales_CRUD(cur, conn, salesperson, all_sales_data, display_data):
             new_amount = st.number_input("Sales Amount", min_value=0)
             submitted = st.form_submit_button("Add Sale")
 
+            # Button submitted
             if submitted:
                 try:
                     cur.execute(
@@ -57,70 +60,86 @@ def Sales_CRUD(cur, conn, salesperson, all_sales_data, display_data):
                     history_update(cur, conn, "Sales", new_sale_id, "Sales", "Insert", "-", new_amount)
 
                     st.success("New sale record added successfully!")
+
                 except Exception as e:
                     st.error(f"Insert failed: {e}")
 
-    # Update record
+    # Update record to the table
     with update_record:
         st.subheader("Update Sale Record")
 
+        # Display & selected data
         selected_update_data = st.selectbox("Select Sale", all_sales_data["SalesID"].tolist(), key="update_selectbox")
         current_data = all_sales_data.loc[all_sales_data['SalesID'] == selected_update_data]
         st.dataframe(display_data.loc[display_data['Sales ID'] == current_data['SalesID'].values[0]])
 
+        # Form to update record
         with st.form("Update Record"):
 
-            # Update form
             update_salesperson = st.selectbox("Sales Person ID", salesperson, index = salesperson.index(current_data['SalesPersonID'].values[0]))
             update_quantity = st.number_input("Number of product", value = current_data['Quantity'].values[0])
             update_year = st.selectbox("Year", year, index = year.index(current_data['Year'].values[0]))
             update_month = st.selectbox("Month", month, index = month.index(current_data['Month'].values[0]))
             new_amount = st.number_input("Sales Amount", value = current_data['Sales'].values[0], min_value = 0)
 
+            # Button submitted
             if st.form_submit_button("Update Record"):
                 try:
+
                     if update_salesperson != current_data['SalesPersonID'].values[0]:
                         cur.execute("UPDATE sales SET salespersonid = %s WHERE salesid = %s",
                                     (update_salesperson, selected_update_data))
                         history_update(cur, conn, "Sales", selected_update_data, "SalesPersonID",
                                        "Update", current_data['SalesPersonID'].values[0], update_salesperson)
+
                     if update_quantity != current_data['Quantity'].values[0]:
                         cur.execute("UPDATE sales SET quantity = %s WHERE salesid = %s",
                                     (update_quantity, selected_update_data))
                         history_update(cur, conn, "Sales", selected_update_data, "Quantity",
                                        "Update", current_data['Quantity'].values[0], update_quantity)
+
                     if update_year != current_data['Year'].values[0]:
                         cur.execute("UPDATE sales SET year = %s WHERE salesid = %s",
                                     (update_year, selected_update_data))
                         history_update(cur, conn, "Sales", selected_update_data, "Year",
                                        "Update", current_data['Year'].values[0], update_year)
+
                     if update_month != current_data['Month'].values[0]:
                         cur.execute("UPDATE sales SET month = %s WHERE salesid = %s",
                                     (update_month, selected_update_data))
                         history_update(cur, conn, "Sales", selected_update_data, "Month",
                                        "Update", current_data['Month'].values[0], update_month)
+
                     if new_amount != current_data['Sales'].values[0]:
                         cur.execute("UPDATE sales SET sales = %s WHERE salesid = %s",
                                     (new_amount, selected_update_data))
                         history_update(cur, conn, "Sales", selected_update_data, "Sales",
                                        "Update", current_data['Sales'].values[0], new_amount)
+
                     conn.commit()
                     st.success("Record updated successfully!")
+
                 except Exception as e:
                     st.error(f"Update failed: {e}")
 
-    # Delete record
+    # Delete record from the table
     with delete_record:
         st.subheader("Delete Sale Record")
 
+        # Display & selected data
         selected_delete_data = st.selectbox("Select Sale", all_sales_data["SalesID"].tolist(), key="delete_selectbox")
         st.dataframe(display_data.loc[display_data['Sales ID'] == selected_delete_data])
 
+        # Button submitted
         if st.button("Delete Record"):
             try:
+
                 cur.execute("DELETE FROM sales WHERE salesid = %s", (selected_delete_data,))
                 history_update(cur, conn, "Sales", selected_delete_data, "-", "Delete", "-", "-")
+
                 conn.commit()
+
                 st.warning("Record deleted successfully!")
+
             except Exception as e:
                 st.error(f"Delete failed: {e}")
